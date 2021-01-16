0e8f98957ff168c3954076c7d71549731e35eb1a
omerbd21 <omerbendavid21@gmail.com>
2021-01-16 19:41:56 +0200

    exercise 1.5 finished

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -25,29 +25,31 @@ const Total = (props) => {
 
 
 const App = () => {
-    const course = 'Half Stack application development'
-    const parts = [
-        {
-            name: 'Fundamentals of React',
-            exercises: 10
-        },
-        {
-            name: 'Using props to pass data',
-            exercises: 7
-        },
-        {
-            name: 'State of a component',
-            exercises: 14
-        }
-    ]
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
 
     return (
         <div>
-            <Header sentence={course} />
-            <Part part={parts[0].name} exercises={parts[0].exercises} />
-            <Part part={parts[1].name} exercises={parts[1].exercises} />
-            <Part part={parts[2].name} exercises={parts[2].exercises} />
-            <Total total={parts[0].exercises+parts[1].exercises+parts[2].exercises} />
+            <Header sentence={course.name} />
+            <Part part={course.parts[0].name} exercises={course.parts[0].exercises} />
+            <Part part={course.parts[1].name} exercises={course.parts[1].exercises} />
+            <Part part={course.parts[2].name} exercises={course.parts[2].exercises} />
+            <Total total={course.parts[0].exercises+course.parts[1].exercises+course.parts[2].exercises} />
         </div>
     )
 }
