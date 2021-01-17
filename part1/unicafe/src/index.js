459825e927d75bfce521a851ba6cb86d3cfb2efc
omerbd21 <omerbendavid21@gmail.com>
2021-01-17 17:10:15 +0200

    finished 1.10

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,22 +1,10 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Statistics = (props) => {
-    const all = props.good+props.neutral+props.bad
-    const average = (props.good + props.bad*-1) / all
-    const positive = props.good / all * 100
+
+const Statistic = (props) => {
     return (
-        isNaN(average) ? <div><h1>statistics</h1>
-                <p>No feedback given</p></div> :
-        <div>
-            <h1>statistics</h1>
-            <p>good {props.good}</p>
-            <p>neutral {props.neutral}</p>
-            <p>bad {props.bad}</p>
-            <p>all {all}</p>
-            <p>average {average}</p>
-            <p>positive {positive}%</p>
-        </div>
+            <p>{props.name} {props.stat}</p>
     )
 }
 
@@ -25,13 +13,22 @@ const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    const all = good+neutral+bad
+    const average = (good + bad*-1) / all
+    const positive = good / all * 100
     return (
         <div>
             <h1>give feedback</h1>
             <button onClick={() => setGood(good+1)}> good</button>
             <button onClick={() => setNeutral(neutral+1)}> neutral</button>
             <button onClick={() => setBad(bad+1)}> bad</button>
-            <Statistics good={good} neutral={neutral} bad={bad} />
+            <h1>statistics</h1>
+            <Statistic name={"good"} stat={good} />
+            <Statistic name={"bad"} stat={bad} />
+            <Statistic name={"neutral"} stat={neutral} />
+            <Statistic name={"all"} stat={all} />
+            <Statistic name={"average"} stat={isNaN(average) ? 0:average} />
+            <Statistic name={"positive"} stat={isNaN(positive) ? 0:positive} />
         </div>
     )
 }
