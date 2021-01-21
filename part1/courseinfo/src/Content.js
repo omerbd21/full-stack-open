7896c394582c66ad553ad19d091a5469bd1af6c4
omerbd21 <omerbendavid21@gmail.com>
2021-01-21 20:10:51 +0200

    finished 2.2 + 2.3

--- part1/courseinfo/src/Content.js
+++ part1/courseinfo/src/Content.js
@@ -1,11 +1,16 @@
 import Part from "./Part";
 import React from "react";
+import Sum from "./Sum";
 
 const Content = (props) => {
     return (
         <div>{props.parts.map(part =>
             <li key={part.id}><Part name={part.name} exercises={part.exercises} /></li>
-        )}</div>
+        )}
+        <li><Sum exercises={props.parts.map(part=> part.exercises).reduce(
+            (previousScore, currentScore)=>previousScore+currentScore,
+            0)} /></li>
+        </div>
     )
 }
 export default Content;
---
+++ part1/courseinfo/src/Sum.js
@@ -0,0 +1,7 @@
+const Sum = (props) => {
+    return (
+        <p> Total number of exercises: {props.exercises}</p>
+    )
+}
+
+export default Sum;
