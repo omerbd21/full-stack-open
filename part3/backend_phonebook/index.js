f46859bf31d1ca8f6c7a1387b62b7a6ac7122f8a
omerbd21 <omerbendavid21@gmail.com>
2021-01-29 18:11:55 +0200

    finished phonebook 3.9

--- part3/backend_phonebook/index.js
+++ part3/backend_phonebook/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const cors = require('cors')
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan('tiny'));
-
+app.use(cors())
+app.use(express.static('build'))
 
 let persons = [{
     "name": "Arto Hellas",
