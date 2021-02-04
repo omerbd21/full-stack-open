4d9370ef67a617854ca8b5ccc575c718d364160e
omerbd21 <omerbendavid21@gmail.com>
2021-02-04 20:30:50 +0200

    finished helper 4.5

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -1,4 +1,9 @@
 const dummy = (blogs) => {
     return 1
 }
-module.exports = {dummy}
+const totalLikes = (blogs) => {
+    if (blogs.length === 1) { return blogs[0].likes}
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return blogs.reduce(reducer)
+}
+module.exports = {dummy, totalLikes}
