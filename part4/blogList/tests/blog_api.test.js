6767ea452f1f8d2ddb5da8f5414d32cb1d2ffb65
omerbd21 <omerbendavid21@gmail.com>
2021-02-05 16:24:28 +0200

    finished blogs 4.9

--- part4/blogList/tests/blog_api.test.js
+++ part4/blogList/tests/blog_api.test.js
@@ -4,7 +4,7 @@ const app = require('../app')
 
 const api = supertest(app)
 
-test('notes are returned as json', async () => {
+test('blogs are returned as json', async () => {
     await api
         .get('/api/blogs')
         .expect(200)
@@ -12,6 +12,14 @@ test('notes are returned as json', async () => {
     jest.setTimeout(30000);
 
 })
+test('there is an id to a blog', async () => {
+    const blogs = await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/).expect(blogs => {if (blogs.length > 0) {blogs[0]._id}})
+    jest.setTimeout(30000);
+});
+
 
 afterAll(() => {
     mongoose.connection.close()
