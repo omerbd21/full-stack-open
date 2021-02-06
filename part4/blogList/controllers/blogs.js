ee57b0dae82ea24c60b58380787d60af292e7c21
omerbd21 <omerbendavid21@gmail.com>
2021-02-06 11:52:32 +0200

    finished 4.13-14 blogs

--- part4/blogList/controllers/blogs.js
+++ part4/blogList/controllers/blogs.js
@@ -7,9 +7,45 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-    const blog = new Blog(request.body)
+    const blog = new Blog({...request.body, likes: request.body.likes ? request.body.likes : 0})
+    try {
+        const result = await blog.save()
+        response.status(201).json(result)
+    }
+    catch (err) {
+        next(err)
+        response.status(400)
+    }
+})
+blogsRouter.put('/:id', async (request, response) => {
+    try {
+        const body = request.body
+        const id = request.params.id
+        const updatedBlog = {
+            title: body.title,
+            author: body.author,
+            url: body.url,
+            likes: body.likes ? body.likes : 0,
+            ...Blog.find({_id:id})
+        }
 
-    const result = await blog.save()
-    response.status(201).json(result)
+        await Blog.findByIdAndUpdate(request.params.id, updatedBlog, { new: true })
+
+        response.status(200).json(updatedBlog)
+    }
+    catch (err) {
+        next(err)
+        response.status(400)
+    }
+})
+blogsRouter.delete('/:id', async (request, response) => {
+    try {
+        await Blog.findByIdAndRemove(request.params.id)
+        response.status(204).end()
+    }
+    catch (err) {
+        next(err)
+        response.status(400)
+    }
 })
 module.exports = blogsRouter
--- part4/blogList/tests/blog_api.test.js
+++ part4/blogList/tests/blog_api.test.js
@@ -19,7 +19,7 @@ test('there is an id to a blog', async () => {
         .expect('Content-Type', /application\/json/).expect(blogs => {if (blogs.length > 0) {blogs[0]._id}})
     jest.setTimeout(30000);
 });
-test('blogs are returned as json', async () => {
+test('blogs are added correctly', async () => {
     const blogs = await api
         .get('/api/blogs')
         .expect(200)
@@ -29,6 +29,22 @@ test('blogs are returned as json', async () => {
         .post('/api/blogs')
         .expect(201)
         .expect('Content-Type', /application\/json/).expect(blogs => blogs.length === oldLength + 1)
+    jest.setTimeout(30000);
+
+})
+test('if no likes, there is none', async () => {
+    await api
+        .post('/api/blogs')
+        .expect(201)
+        .expect('Content-Type', /application\/json/).expect(request => request.body.likes === 0  )
+    jest.setTimeout(30000);
+
+})
+test('if no url or name, return 400', async () => {
+    await api
+        .post('/api/blogs').set('Body', '{}')
+        .expect(400)
+        .expect('Content-Type', /application\/json/)
     jest.setTimeout(30000);
 
 })
