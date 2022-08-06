c487d2c831a4e973047e2a233a03e76f6dff74c1
omerbd21 <>
2022-08-06 11:27:31 +0300

    finished 2.1

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -17,7 +17,7 @@ const Content = ({parts}) => {
 
   return (
     <div>
-      { parts.map(part => <Part part={part} />)}
+      { parts.map(part => <Part part={part} key={part.id} />)}
     </div>
     
   )
@@ -31,6 +31,13 @@ const Total = ({parts}) => {
     <p>Number of exercises {exercises}</p>
   )
 }
+const Course = ({course}) => {
+  return (<div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>)
+}
 
 const App = () => {
   const course = { 
@@ -38,23 +45,22 @@ const App = () => {
   parts: [
     {
       name: 'Fundamentals of React',
-      exercises: 10
+      exercises: 10,
+      id: 1
     }, 
     {
       name: 'Using props to pass data',
-      exercises: 7
+      exercises: 7,
+      id: 2
     }, 
     {
       name: 'State of a component',
-      exercises: 14
+      exercises: 14,
+      id: 3
     }
   ]}
   return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
+    <Course course={course} />    
   )
 }
 
